f385fe2343c5b874c36dacbdf1b2ec8321b557ec
ahmadfebrianmaulana59-droid <ahmadfebrianmaulana59@gmail.com>
2025-10-23 10:12:51 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID-nya
+const tombolsapa =document.getE1lementById('sapaButton');
+// 2. Tambahkan ' event listener' untuk aksi 'click'
+tombolSapa.addEventListener('click',function() {
+    // 3.Jalankan kode ini ketika tombol di-klik
+    alert('Haslo! Terima kassih sudah berkunjung !)';
+});
